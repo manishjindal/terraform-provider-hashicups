1782cb12ce20564f20fe476e4c11318538aa57f0
Manish Jindal <jindal.manishkumar1@gmail.com>
2024-09-21 03:03:01 +0530

    fix: build error

--- internal/provider/coffee_resource.go
+++ internal/provider/coffee_resource.go
@@ -16,8 +16,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
-	// "github.com/hashicorp/terraform-plugin-framework/resource"
-	// "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 )
 
 // Ensure provider defined types fully satisfy framework interfaces.
