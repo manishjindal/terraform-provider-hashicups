4c4e4f8af1c79c941f8271485985d73f3c4ff7a6
Manish Jindal <jindal.manishkumar1@gmail.com>
2024-09-21 03:34:43 +0530

    remove accp tests

--- internal/provider/coffee_data_source_test.go
+++ internal/provider/coffee_data_source_test.go
@@ -3,30 +3,24 @@
 
 package provider
 
-import (
-	"testing"
+// func TestAccExampleDataSource(t *testing.T) {
+// 	resource.Test(t, resource.TestCase{
+// 		PreCheck:                 func() { testAccPreCheck(t) },
+// 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+// 		Steps: []resource.TestStep{
+// 			// Read testing
+// 			{
+// 				Config: testAccExampleDataSourceConfig,
+// 				Check: resource.ComposeAggregateTestCheckFunc(
+// 					resource.TestCheckResourceAttr("data.hashicups_coffees.test", "id", "example"),
+// 				),
+// 			},
+// 		},
+// 	})
+// }
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-)
-
-func TestAccExampleDataSource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Read testing
-			{
-				Config: testAccExampleDataSourceConfig,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.hashicups_coffees.test", "id", "example"),
-				),
-			},
-		},
-	})
-}
-
-const testAccExampleDataSourceConfig = `
-data "hashicups_coffees" "test" {
-  configurable_attribute = "example"
-}
-`
+// const testAccExampleDataSourceConfig = `
+// data "hashicups_coffees" "test" {
+//   configurable_attribute = "example"
+// }
+// `
